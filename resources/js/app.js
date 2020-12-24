e474ff77d4f1527e121e57ef35983f66d24f4f66
Gleb Deriyenko <gleb.deriyenko@gmail.com>
2020-12-24 18:42:59 +0200

    Added auth process and users seeds. Vuejs installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,25 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+window.Vue = require("vue");
+
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
+new Vue({
+    vuetify,
+  }).$mount('#app')
+const vuetify = new Vuetify({
+    icons: {
+        iconfont: "mdi" // default - only for display purposes
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: "#2CCE7A"
+            }
+        }
+    }
+});
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Tools') }}</div>
+
+                <div class="card-body">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFile">
+                        <label class="custom-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
